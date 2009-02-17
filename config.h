b52b4d9af0c7d3c014edfb84402d59ab486ca5f6
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-02-17 05:43:34 +0000

    Updated regular expression to match Subversion's keyword pattern. Added missing depency on config.h.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@69 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -95,7 +95,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "66"
+#define VCS_REVISION "68"
 
 /* Version number of package */
 #define VERSION "2.4"
