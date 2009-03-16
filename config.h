5cea9a25d849a4dde5522f66fe0943ae31ec45b3
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-03-16 15:12:02 +0000

    Updated version
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@76 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -95,7 +95,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "69"
+#define VCS_REVISION "76"
 
 /* Version number of package */
 #define VERSION "2.4"
