6021dd8aaad4d71142936ec8f0b532a86307da42
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-03-30 17:34:36 +0000

    Force updating revision numbers in Javascript files, when running the commit script.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@103 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -95,7 +95,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "102"
+#define VCS_REVISION "103"
 
 /* Version number of package */
 #define VERSION "2.5"
