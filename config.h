6dbbe62310a1c195a14784b29ac96f83f8770927
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-06-21 19:41:32 +0000

    Added code so that "objcopy" picks an appropriate target architecture when building for ARM.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@124 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -132,7 +132,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "123"
+#define VCS_REVISION "124"
 
 /* Version number of package */
 #define VERSION "2.8"
--- shellinabox/shell_in_a_box.js
+++ shellinabox/shell_in_a_box.js
@@ -355,7 +355,7 @@ ShellInABox.prototype.extendContextMenu = function(entries, actions) {
 };
 
 ShellInABox.prototype.about = function() {
-  alert("Shell In A Box version " + "2.8 (revision 123)" +
+  alert("Shell In A Box version " + "2.8 (revision 124)" +
         "\nCopyright 2008-2009 by Markus Gutschke\n" +
         "For more information check http://shellinabox.com" +
         (typeof serverSupportsSSL != 'undefined' && serverSupportsSSL ?
