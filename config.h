8ff79d1ce7468d0a7f0d6ae440fe44dadfb5744a
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-07-06 04:28:59 +0000

    Released version 2.9
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@139 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -23,7 +23,7 @@
 #define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <libutil.h> header file. */
-#define HAVE_LIBUTIL_H 1
+/* #undef HAVE_LIBUTIL_H */
 
 /* Define to 1 if you have the `login_tty' function. */
 #define HAVE_LOGIN_TTY 1
@@ -120,22 +120,22 @@
 #define PACKAGE_NAME "shellinabox"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "shellinabox 2.8"
+#define PACKAGE_STRING "shellinabox 2.9"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "shellinabox"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.8"
+#define PACKAGE_VERSION "2.9"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "137"
+#define VCS_REVISION "139"
 
 /* Version number of package */
-#define VERSION "2.8"
+#define VERSION "2.9"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
