d96e312a70bea43948d8bcbe1e68085a75b4c964
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-07-08 17:08:47 +0000

    More fine tuning of the regular expressions that detect URLs. We now allow '.' and ',' in URLs, as long as they are not at the very end of the URL, where they would probably be part of the enclosing sentence and not actually part of the URL.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@143 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -135,7 +135,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "142"
+#define VCS_REVISION "143"
 
 /* Version number of package */
 #define VERSION "2.9"
