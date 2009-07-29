01e6f3c9de8a2c43c95ce80a5b1f37fc8fb7f765
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-07-29 21:08:06 +0000

    Updated manual page to show new SSH service.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@155 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -135,7 +135,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "153"
+#define VCS_REVISION "155"
 
 /* Version number of package */
 #define VERSION "2.9"
