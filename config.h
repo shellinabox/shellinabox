bc811017fd41723b4225e819b368e86a5808488e
zodiac <zodiac@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-08-12 19:55:25 +0000

    Allow quotes in SHELLINABOX_ARGS (for Debian packages)
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@168 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "167"
+#define VCS_REVISION "168"
 
 /* Version number of package */
 #define VERSION "2.9"
--- shellinabox/shell_in_a_box.js
+++ shellinabox/shell_in_a_box.js
@@ -355,7 +355,7 @@ ShellInABox.prototype.extendContextMenu = function(entries, actions) {
 };
 
 ShellInABox.prototype.about = function() {
-  alert("Shell In A Box version " + "2.9 (revision 167)" +
+  alert("Shell In A Box version " + "2.9 (revision 168)" +
         "\nCopyright 2008-2009 by Markus Gutschke\n" +
         "For more information check http://shellinabox.com" +
         (typeof serverSupportsSSL != 'undefined' && serverSupportsSSL ?
