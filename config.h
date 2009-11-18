cd126f40f9135fd38a513bc2986b7dff470fe6b7
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-18 22:58:06 +0000

    Fix handling of control and capslock keys.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@182 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "181"
+#define VCS_REVISION "182"
 
 /* Version number of package */
 #define VERSION "2.9"
