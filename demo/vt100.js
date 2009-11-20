9eb1693630ebab4ffdf7ff80161305cf30400e09
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-20 18:51:42 +0000

    Add dependency on lsb-base for the Debian package
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@183 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- demo/vt100.js
+++ demo/vt100.js
@@ -1901,7 +1901,7 @@ VT100.prototype.toggleBell = function() {
 };
 
 VT100.prototype.about = function() {
-  alert("VT100 Terminal Emulator " + "2.9 (revision 182)" +
+  alert("VT100 Terminal Emulator " + "2.9 (revision 183)" +
         "\nCopyright 2008-2009 by Markus Gutschke\n" +
         "For more information check http://shellinabox.com");
 };
