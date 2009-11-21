3c644e1c4c1fd57d6f57629c503edc673ed9fbf7
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-21 21:01:32 +0000

    Document the --user-css command line option. Added an example on how to configure Apache for reverse proxy operation.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@185 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "183"
+#define VCS_REVISION "185"
 
 /* Version number of package */
 #define VERSION "2.9"
