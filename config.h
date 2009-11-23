99169cff6ab8c296595df47360a637733d2cb53f
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-23 04:57:27 +0000

    Thanks to Rudolf Spring's help, added support for building on PowerPC architectures.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@189 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "188"
+#define VCS_REVISION "189"
 
 /* Version number of package */
 #define VERSION "2.10"
