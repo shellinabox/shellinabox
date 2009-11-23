c64e8aeb979f8f3af86108d7ac605ffe99e50b4e
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-23 04:53:38 +0000

    Added some documentation explaining how to configure ShellInABox on Fedora.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@188 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "187"
+#define VCS_REVISION "188"
 
 /* Version number of package */
 #define VERSION "2.10"
