7862fb99be14a4d5213daa6092dafd007ff9bc30
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-25 23:56:56 +0000

    Enable switching between 80 and 132 column mode, if the browser supports CSS transforms.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@190 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "189"
+#define VCS_REVISION "190"
 
 /* Version number of package */
 #define VERSION "2.10"
--- shellinabox/shell_in_a_box.js
+++ shellinabox/shell_in_a_box.js
@@ -358,7 +358,7 @@ ShellInABox.prototype.extendContextMenu = function(entries, actions) {
 };
 
 ShellInABox.prototype.about = function() {
-  alert("Shell In A Box version " + "2.10 (revision 189)" +
+  alert("Shell In A Box version " + "2.10 (revision 190)" +
         "\nCopyright 2008-2009 by Markus Gutschke\n" +
         "For more information check http://shellinabox.com" +
         (typeof serverSupportsSSL != 'undefined' && serverSupportsSSL ?
