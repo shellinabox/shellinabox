03886076e19221467319c3cba61d4db0cd31d2ff
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-11-26 21:05:46 +0000

    Lock the Debian compatibility level a 6, so that the packages can easily be built on Hardy.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@191 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "190"
+#define VCS_REVISION "191"
 
 /* Version number of package */
 #define VERSION "2.10"
