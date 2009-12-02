cc4caca98229e5598c15f6fbe2b742ff66233d2a
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2009-12-02 04:32:09 +0000

    * Added checks that the debian/changelog file is up-to-date
    * Added debian/watch file
    
    * Updated the Standards-Version in debian/control. Still need to
      review whether we are in full compliance, though.
    
    
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@194 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -138,7 +138,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "193"
+#define VCS_REVISION "194"
 
 /* Version number of package */
 #define VERSION "2.10"
