445d3c66a6071b2534d81e691a15ec2988a9e09b
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2010-07-09 17:22:26 +0000

    Another attempt at coming up with Makefile rules that work for both Linux and BSD.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@210 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -153,7 +153,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "209"
+#define VCS_REVISION "210"
 
 /* Version number of package */
 #define VERSION "2.10"
