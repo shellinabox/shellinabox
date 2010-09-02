4db3ed8797f7580087c549ab7f1aff469fa22a3f
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2010-09-02 23:42:42 +0000

    Force a zero pixel margin on the <body> tag. Without this change, ShellInABox would occasionally think that it was in "embedded" mode, whenever the user applied a scaling factor (e.g. by pressing CTRL-Minus).
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@217 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -153,7 +153,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "216"
+#define VCS_REVISION "217"
 
 /* Version number of package */
 #define VERSION "2.10"
