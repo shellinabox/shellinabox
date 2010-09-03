daffdc356a13374094e112746ab990f416a6b753
zodiac@gmail.com <zodiac@gmail.com@0da03de8-d603-11dd-86c2-0f8696b7b6f9>
2010-09-03 17:24:57 +0000

    Fix some scaling related issues. This fix is thanks to some excellent detective work by Jan <siev...@zedat.fu-berlin.de>.
    git-svn-id: https://shellinabox.googlecode.com/svn/trunk@218 0da03de8-d603-11dd-86c2-0f8696b7b6f9

--- config.h
+++ config.h
@@ -153,7 +153,7 @@
 #define STDC_HEADERS 1
 
 /* Most recent revision number in the version control system */
-#define VCS_REVISION "217"
+#define VCS_REVISION "218"
 
 /* Version number of package */
 #define VERSION "2.10"
