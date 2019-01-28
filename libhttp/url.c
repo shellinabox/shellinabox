4f0ecc31ac6f985e0dd3f5a52cbfc0e9251f6361
Marc Singer <elf@buici.com>
2019-01-27 20:18:48 -0800

    Rolling code for version 2.21

--- libhttp/url.c
+++ libhttp/url.c
@@ -313,6 +313,21 @@ static void urlParsePostBody(struct URL *url,
             }
           }
         }
+        /* elf-2018.09.09: Detection of broken multipart/form-data
+           fixes DoS vulnerability.
+
+           On 9/9/18 10:43 AM, Imre Rad wrote:
+           Hi Markus, Marc!
+
+           I identified a vulnerability today in Shellinabox, it is
+           remote a denial of service, shellinaboxd eating up 100% cpu
+           and not processing subsequent requests after the attack was
+           mounted.
+        */
+        else {
+          warn ("[http] Ignorning broken multipart/form-data");
+          break;
+        }
       }
       if (lastPart) {
         warn("[http] Missing final \"boundary\" for \"multipart/form-data\"!");
